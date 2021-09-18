10d576b80b6cef37515826581f6203415718653d
Rein van der Woerd <reinvanderwoerd@me.com>
2021-09-18 01:53:05 -0700

    Wandering cursor

--- src/main/kotlin/WanderingCursor.kt
+++ src/main/kotlin/WanderingCursor.kt
@@ -1,5 +1,7 @@
 import org.openrndr.application
+import org.openrndr.draw.loadImage
 import org.openrndr.extra.noise.simplex
+import org.openrndr.extras.imageFit.imageFit
 
 
 fun main() = application {
@@ -9,11 +11,13 @@ fun main() = application {
     }
 
     program {
+        val cursor = loadImage("data/images/cursor.png")
 
         extend {
-            val x = simplex(0,  seconds) * .5 * width + width/2
-            val y = simplex(1000,  seconds) * .5 * height + height/2
-            drawer.circle(x, y, 10.0)
+            val x = simplex(0,  .5 * seconds) * .5 * width + width/2
+            val y = simplex(1000, .1*  seconds) * .005 * height + height/2
+//            drawer.circle(x, y, 10.0)
+            drawer.imageFit(cursor, x, y, 20.0, 20.0)
         }
     }
 }
